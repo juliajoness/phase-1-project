1f56787e49e72a23dd15ddd7a6557e6c8544ce8e
Lianzi <lianziliu.sugar@gmail.com>
2022-10-24 17:51:18 -0400

    add first 25 character, and add heart button

--- index.js
+++ index.js
@@ -1,22 +1,81 @@
 const charactersUrl = 'https://hp-api.herokuapp.com/api/characters'
 const characterDiv = document.querySelector('#character-div')
+const reviewForm = document.querySelector('form')
+const reviewInput =  document.querySelector('.review')
+const reviewDiv = document.querySelector('#review-panel')
+const EMPTY_HEART = '♡'
+const FULL_HEART = '♥'
 
 fetch(charactersUrl)
 .then(r => r.json())
-.then(characterArray => {characterArray.forEach (characterObj => {
-    renderCharacters(characterObj)
-    // console.log(characterObj.gender)
-    })
-})
+.then(characterArray =>{
+    //renderCharacters(characterArray)
+    const characterOnThePage = characterArray.splice(0, 25)
+    characterOnThePage.forEach(characterObj=> {
+        renderCharacters(characterObj)
+        //console.log(characterObj)
 
-function renderCharacters (characterObj) {
+    })
+    reviewForm.addEventListener("submit", e => {
+        e.preventDefault()
+        const characterReview = document.createElement('p')
+        characterReview.textContent = reviewInput.value
+        reviewDiv.append(characterReview)
+    })
+    // searchForm.addEventListener('submit', (e) =>{
+    //     e.preventDefault()
+    //     characterDiv.innerHTML = " "
+    //     // searchInput.value
+    //     searchInput.value = characterNameList.textContent
+    //     characterImg.src = characterObj.image
+    //     characterSpecies.textContent = characterObj.species
+    //     characterGender.textContent = characterObj.gender
+    //     characterDiv.append(characterCard)
+    //     characterCard.append(characterNameList,characterImg, characterSpecies, characterGender)
+
+    // })// searchForm Submit Event
+
+    // document.querySelector('img').addEventListener("mouseover", ()=>{
+    //     const funImg = document.createElement("img")
+    //     funImg.src = "./piggy.gif"
+    //     characterDiv.append(funImg)
+    // })
+
+}) // fetch 2nd then
+
+
+
+
+function renderCharacters (characterObj ) {
+
     const characterCard = document.createElement('ul')
-    const characterNameList = document.createElement('li')
-    const characterSpecies = document.createElement('p')
-    const characterGender = document.createElement('p')
+    const characterNameList = document.createElement('h2')
+    const characterImg = document.createElement('img')
+    const characterSpecies = document.createElement('h3')
+    const characterGender = document.createElement('h5')
+    const likeBtn = document.createElement('button')
+
+    // characterNameList.id = characterNameList.dataset.id
     characterNameList.textContent = characterObj.name
+    characterImg.src = characterObj.image
     characterSpecies.textContent = characterObj.species
     characterGender.textContent = characterObj.gender
+    likeBtn.textContent = '♡'
     characterDiv.append(characterCard)
-    characterCard.append(characterNameList, characterSpecies, characterGender)
-}
+    characterCard.append(characterNameList,characterImg, characterSpecies, characterGender, likeBtn)
+
+    // console.log(characterCard)
+    let redHeart = false;
+    likeBtn.addEventListener("click", ()=>{
+        redHeart = !redHeart;
+        if (redHeart){
+            likeBtn.style.color = "red";
+            likeBtn.textContent= FULL_HEART;
+        } else{
+            likeBtn.style.color = "black";
+            likeBtn.textContent = EMPTY_HEART;
+        }
+ })
+
+
+} //renderCharacter function
