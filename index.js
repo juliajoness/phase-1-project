0b42cd35ca579101ec079345b63341d065490d7d
julia <juliajaj3@gmail.com>
2022-10-25 11:43:01 -0400

    new stuff

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const charactersUrl = 'https://hp-api.herokuapp.com/api/characters'
-const characterDiv = document.querySelector('#character-div')
+const characterCard = document.querySelector('.flip-card')
+//const characterEachCard = document.querySelector('#flip-card-inner')
+const characterCardFront = document.querySelector('.flip-card-front')
+const characterCardBack = document.querySelector('.flip-card-back')
+
 const reviewForm = document.querySelector('form')
 const reviewInput =  document.querySelector('.review')
 const reviewDiv = document.querySelector('#review-panel')
@@ -9,16 +13,18 @@ const FULL_HEART = '♥'
 fetch(charactersUrl)
 .then(r => r.json())
 .then(characterArray =>{
-    //renderCharacters(characterArray)
+   // renderCharacters(characterArray)
+
     const characterOnThePage = characterArray.splice(0, 25)
     characterOnThePage.forEach(characterObj=> {
-        renderCharacters(characterObj)
-        //console.log(characterObj)
-        document.querySelector('img').addEventListener("mouseover", ()=>{
-            const funImg = document.createElement("img")
-            funImg.src = "./piggy.gif"
-            characterDiv.append(funImg)
-        })
+        renderCharactersImg(characterObj)
+        const characterEachCard = document.createElement('div')
+        characterEachCard.className = "flip-card-inner"
+        characterEachCard.append(characterCardFront, characterCardBack)
+        characterCard.append(characterEachCard)
+        document.querySelector('img').addEventListener("mouseover", (characterObj)=>{
+            renderCharacters(characterObj)
+       })
     })
     reviewForm.addEventListener("submit", e => {
         e.preventDefault()
@@ -49,23 +55,26 @@ fetch(charactersUrl)
 
 function renderCharacters (characterObj ) {
 
-    const characterCard = document.createElement('ul')
     const characterNameList = document.createElement('h2')
-    const characterImg = document.createElement('img')
     const characterSpecies = document.createElement('h3')
     const characterGender = document.createElement('h5')
-    const likeBtn = document.createElement('button')
+    //characterNameList.id = characterNameList.dataset.id
 
-    // characterNameList.id = characterNameList.dataset.id
     characterNameList.textContent = characterObj.name
-    characterImg.src = characterObj.image
     characterSpecies.textContent = characterObj.species
     characterGender.textContent = characterObj.gender
+    characterCardBack.append(characterNameList, characterSpecies, characterGender)
+}
+//renderCharacter function
+ function renderCharactersImg (characterObj) {
+    const characterImg = document.createElement('img')
+    const likeBtn = document.createElement('button')
+    characterImg.src = characterObj.image
+    characterImg.className = "img"
+    console.log(characterImg.src)
     likeBtn.textContent = '♡'
-    characterDiv.append(characterCard)
-    characterCard.append(characterNameList,characterImg, characterSpecies, characterGender, likeBtn)
-
-    // console.log(characterCard)
+    likeBtn.className = "like-Btn"
+    characterCardFront.append(characterImg, likeBtn)
     let redHeart = false;
     likeBtn.addEventListener("click", ()=>{
         redHeart = !redHeart;
@@ -76,10 +85,8 @@ function renderCharacters (characterObj ) {
             likeBtn.style.color = "black";
             likeBtn.textContent = EMPTY_HEART;
         }
- })
-
-
-} //renderCharacter function
+    })
+ }
 // url("//db.onlinewebfonts.com/t/0421d4186d6efbfc5331fe180895e780.woff2") format("woff2"),
 //     url("//db.onlinewebfonts.com/t/0421d4186d6efbfc5331fe180895e780.woff") format("woff"),
 //     url("//db.onlinewebfonts.com/t/0421d4186d6efbfc5331fe180895e780.ttf") format("truetype"),
